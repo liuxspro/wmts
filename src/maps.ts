14a3e96cca128267c11c281869cd2e100949f7ca
liuxspro <liuxspro@gmail.com>
2025-04-07 22:33:52 +0800

    Feat: add tianditu xz

--- src/maps.ts
+++ src/maps.ts
@@ -2,10 +2,12 @@ import { debug } from "./maps/debug.ts";
 import { cap as google } from "./maps/google_map.ts";
 import { cap as tianditu } from "./maps/tianditu.ts";
 import { cap as tianditu_js } from "./maps/tianditu_js.ts";
+import { cap as xuzhou } from "./maps/tianditu_js_xz.ts";
 
 export const maps = {
   debug,
   google,
   tianditu,
   tianditu_js,
+  xuzhou,
 };
---
+++ src/maps/tianditu_js_xz.ts
@@ -0,0 +1,63 @@
+/**
+ * 天地图徐州 影像对比系统
+ * http://www.xzmap.gov.cn/xuzhou/compereXZ/map/comp.jsp
+ * 测试接口
+ * http://221.229.211.117/DOM/service-tile.jsp
+ *
+ * 瓦片模板
+ * http://221.229.211.117/DOM/wmts/BZ_DOM_24_QS/BZ_DOM_24_QS/BZ_DOM_24_QS_Matrix_0/18/40587/216393.png
+ * http://221.229.211.117/DOM/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=BZ_DOM_24_QS&STYLE=BZ_DOM_24_QS&TILEMATRIXSET=BZ_DOM_24_QS_Matrix_0&TILEMATRIX=18&TILEROW=40587&TILECOL=216393
+ *
+ */
+
+import {
+  CRS_Point,
+  default_service,
+  generate_capabilities,
+  generate_crs84_tile_matrixs,
+  MapLayer,
+  TileMatrixSet,
+} from "../capgen.ts";
+
+const xz_maps = {
+  BZ_DOM_24_QS: "徐州 2024 年影像",
+  BZ_DOM_23_QS: "徐州 2023 年影像",
+  BZ_DOM_22_QS: "徐州 2022 年影像",
+  BZ_DOM_21_QS: "徐州 2021 年影像",
+  BZ_DOM_20_QS: "徐州 2020 年影像",
+  BZ_DOM_19_QS: "徐州 2019 年影像",
+  BZ_DOM_18_QS: "徐州 2018 年影像",
+};
+
+const xz_bbox: [CRS_Point, CRS_Point] = [
+  { lon: 117.0703125, lat: 34.1015625 }, // 西南角 (LowerCorner)
+  { lon: 117.421875, lat: 34.453125 }, // 东北角 (UpperCorner)
+];
+
+const xz_matrix: TileMatrixSet = {
+  title: "CRS84 for Xu Zhou",
+  id: "CGCS2000Quad",
+  supported_crs: "EPSG:4490",
+  wellknown_scale_set:
+    "http://www.opengis.net/def/wkss/OGC/1.0/GoogleCRS84Quad",
+  tile_matrixs: generate_crs84_tile_matrixs(11, 18),
+};
+
+const xz_layers: MapLayer[] = [];
+
+Object.entries(xz_maps).forEach(([key, name]) => {
+  xz_layers.push(
+    new MapLayer(
+      name,
+      name,
+      key,
+      xz_bbox,
+      "CGCS2000Quad",
+      `http://221.229.211.117/DOM/wmts/${key}/${key}/${key}_Matrix_0/{z}/{y}/{x}.png`
+    )
+  );
+});
+
+export const cap = generate_capabilities(default_service, xz_layers, [
+  xz_matrix,
+]);
