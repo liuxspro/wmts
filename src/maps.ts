bf511eaeddf51b2576df972da74bdbee61afe54c
liuxspro <liuxspro@gmail.com>
2025-05-11 13:39:15 +0000

    Chore: remove tiantidu from dist

--- src/maps.ts
+++ src/maps.ts
@@ -1,6 +1,5 @@
 import { debug } from "./maps/debug.ts";
 import { cap as google } from "./maps/google_map.ts";
-import { cap as tianditu } from "./maps/tianditu.ts";
 import { cap as tianditu_js } from "./maps/tianditu_js.ts";
 import { cap as xuzhou } from "./maps/tianditu_js_xz.ts";
 import { cap as osm } from "./maps/osm.ts";
@@ -8,7 +7,6 @@ import { cap as osm } from "./maps/osm.ts";
 export const maps = {
   debug,
   google,
-  tianditu,
   tianditu_js,
   xuzhou,
   osm,
