b89779caf915f7c83618a915c1d0cebd9a90ad90
liuxspro <liuxspro@gmail.com>
2026-01-30 11:30:51 +0800

    Chore: add libs

--- src/server/tiles/bing.ts
+++ src/server/tiles/bing.ts
@@ -1,25 +1,12 @@
 import { Hono } from "hono";
-
-function tileToQuadkey(x: number, y: number, z: number) {
-  if (z === 0) {
-    return ""; // 缩放级别0的quadkey为空字符串
-  }
-  let quadkey = "";
-  for (let i = z; i > 0; i--) {
-    let digit = 0;
-    const mask = 1 << (i - 1);
-    if ((x & mask) !== 0) digit += 1;
-    if ((y & mask) !== 0) digit += 2;
-    quadkey += digit;
-  }
-  return quadkey;
-}
+import { XYZ } from "@liuxspro/libs/geo";
 
 export const router = new Hono();
 
 router.get("/tile/bing/:z/:x/:y", (c) => {
   const { z, x, y } = c.req.param();
-  const quadkey = tileToQuadkey(parseInt(x), parseInt(y), parseInt(z));
+  const xyz = new XYZ(parseInt(x), parseInt(y), parseInt(z));
+  const quadkey = xyz.to_bing_quadkey();
   const redirect =
     `https://ecn.t3.tiles.virtualearth.net/tiles/a${quadkey}.jpeg?g=0&dir=dir_n`;
 
