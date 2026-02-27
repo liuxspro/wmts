8d43e80cc270cd64c1b38fa971421ab3a85725b9
liuxspro <liuxspro@gmail.com>
2026-02-27 19:40:57 +0800

    Chore: handle restful request

--- src/server/collection.ts
+++ src/server/collection.ts
@@ -3,7 +3,16 @@ import { collection } from "../maps/collection.ts";
 
 const app = new Hono();
 
+// KVP
+// ?SERVICE=WMTS&REQUEST=GetCapabilities&VERSION=1.0.0
 app.get("/", (c) => {
+  c.header("Content-Type", "text/xml;charset=UTF-8");
+  return c.body(collection);
+});
+
+// RESTful
+// /1.0.0/WMTSCapabilities.xml
+app.get("/1.0.0/WMTSCapabilities.xml", (c) => {
   c.header("Content-Type", "text/xml;charset=UTF-8");
   return c.body(collection);
 });
