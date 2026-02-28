dffe3874e0800edbc77e77964e3fc46908d8a358
liuxspro <liuxspro@gmail.com>
2026-02-28 21:47:21 +0800

    Chore: update index page

--- src/server.ts
+++ src/server.ts
@@ -6,17 +6,29 @@ import { router as geocloud_router } from "./server/geocloud.ts";
 import { router as tianditu_router } from "./server/tianditu.ts";
 import { router as jl1_router } from "./server/jilin1.ts";
 import bing from "./server/tiles/bing.ts";
+import osm from "./server/osm.ts";
 
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.html(`
-<a href="/collection"  target="_blank">wmts map collection</a><br>
-<code>On Deno Deploy 💖</code>
+<ul>
+  <li><a href="/osm"  target="_blank">osm</a></li>
+  <li><a href="/collection"  target="_blank">wmts map collection</a></li>
+</ul>
+<div style="
+  text-align: center;
+  position: fixed;
+  bottom: 10px;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+">
+  <code>On Deno Deploy 💖</code>
+</div>
+
 `);
 });
-
-app.use("/index.html", serveStatic({ root: "./src" }));
 
 app.use("/dist/*", serveStatic({ root: "./" }));
 app.use(trimTrailingSlash());
@@ -25,6 +37,7 @@ app.route("/collection", collection);
 app.route("/geocloud", geocloud_router);
 app.route("/tianditu", tianditu_router);
 app.route("/jl1", jl1_router);
+app.route("/osm", osm);
 app.route("/", bing);
 
 Deno.serve(app.fetch);
