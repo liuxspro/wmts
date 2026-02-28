f34f84d9ee96450fd45a64b916202aaa1d3a9cff
liuxspro <liuxspro@gmail.com>
2026-02-28 21:48:31 +0800

    Refactor: use multiple paths

--- src/server/collection.ts
+++ src/server/collection.ts
@@ -3,16 +3,11 @@ import { collection } from "../maps/collection.ts";
 
 const app = new Hono();
 
-// KVP
-// ?SERVICE=WMTS&REQUEST=GetCapabilities&VERSION=1.0.0
-app.get("/", (c) => {
-  c.header("Content-Type", "text/xml;charset=UTF-8");
-  return c.body(collection);
-});
-
-// RESTful
-// /1.0.0/WMTSCapabilities.xml
-app.get("/1.0.0/WMTSCapabilities.xml", (c) => {
+/**
+ * RESTful /1.0.0/WMTSCapabilities.xml
+ * KVP ?SERVICE=WMTS&REQUEST=GetCapabilities&VERSION=1.0.0
+ */
+app.on("GET", ["/", "/1.0.0/WMTSCapabilities.xml"], (c) => {
   c.header("Content-Type", "text/xml;charset=UTF-8");
   return c.body(collection);
 });
