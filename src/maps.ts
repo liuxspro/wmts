730f9e044df97814511bf048905daf769f26c0da
liuxspro <liuxspro@gmail.com>
2026-03-06 16:58:33 +0800

    Fix: import esri

--- src/maps.ts
+++ src/maps.ts
@@ -5,7 +5,7 @@ import { cap as xuzhou } from "./maps/tianditu/jiangsu_xuzhou.ts";
 import { cap as shipxy } from "./maps/shipxy.ts";
 import { cap as amap } from "./maps/amap.ts";
 import { cap as beijing } from "./maps/tianditu/beijing.ts";
-import { cap as esri } from "./maps/esri.ts";
+import esri from "./maps/esri.ts";
 import { cap as gggis } from "./maps/gggis.ts";
 import { cap as jl } from "./maps/tianditu/jilin.ts";
 import { cap as yandex } from "./maps/yandex.ts";
