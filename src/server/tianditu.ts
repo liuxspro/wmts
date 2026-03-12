4d164d224eee3c3a936e3ad16e6fb5633220cd71
liuxspro <liuxspro@gmail.com>
2026-03-12 10:41:20 +0800

    Refactor: update tianditu

--- src/server/tianditu.ts
+++ src/server/tianditu.ts
@@ -6,25 +6,24 @@ import { cap as 江苏 } from "../maps/tianditu/jiangsu.ts";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  const tk_name = "tdt"; // 如果用tk, arcgis 设置的自定义参数会导致瓦片URL tk 重复
-  const tdt_tk = c.req.header(tk_name) || c.req.query(tk_name);
-  if (tdt_tk) {
-    // 创建图层副本并设置token
-    let token = tdt_tk;
-    // 处理请求参数后携带 `/1.0.0/WMTSCapabilities.xml`的情况（arcgis 10.2）
-    if (tdt_tk.includes("/")) {
-      token = tdt_tk.split("/")[0];
-    }
-    // 处理请求参数后携带 `?SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetCapabilities`的情况（arcgis 10.2）
-    if (tdt_tk.includes("?")) {
-      token = tdt_tk.split("?")[0];
-    }
-    c.header("Content-Type", "text/xml;charset=UTF-8");
-    return c.body(tianditu_cap(token));
-  } else {
-    return c.text("Tianditu token is required", 400);
+/**
+ * Tianditu WMTS Capabilities endpoint
+ * Accepts a token via header or query parameter "tk" or "tdt"
+ * Example: /tianditu?tdt=123&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetCapabilities
+ * Example: /tianditu/1.0.0/WMTSCapabilities.xml?tk=123&
+ */
+app.on("GET", ["/", "/1.0.0/WMTSCapabilities.xml"], (c) => {
+  const tdt = c.req.header("tdt") || c.req.query("tdt");
+  const tk = c.req.header("tk") || c.req.query("tk");
+  const tdt_tk_final = tdt || tk;
+  if (!tdt_tk_final) {
+    return c.text(
+      "Tianditu token is required, Accepts token via header or query parameter 'tk' or 'tdt'",
+      400,
+    );
   }
+  c.header("Content-Type", "text/xml;charset=UTF-8");
+  return c.body(tianditu_cap(tdt_tk_final));
 });
 
 app.get("/sdhis/:id/:el", (c) => {
