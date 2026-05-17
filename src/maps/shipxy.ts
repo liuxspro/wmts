641f0fbdae04aac31c0710231214fc2f5057d60f
liuxspro <liuxspro@gmail.com>
2026-05-17 09:58:11 +0800

    Remove duplicate world_mercator_bbox declarations

--- src/maps/shipxy.ts
+++ src/maps/shipxy.ts
@@ -1,15 +1,10 @@
 import {
   Capabilities,
-  GeoPoint,
   MapLayer,
   Service,
+  world_mercator_bbox,
   world_mercator_quad,
 } from "@liuxspro/capgen";
-
-export const world_mercator_bbox: [GeoPoint, GeoPoint] = [
-  { lon: -180.0, lat: -85.08405903 }, // 西南角 (LowerCorner)
-  { lon: 180.0, lat: 85.08405903 }, // 东北角 (UpperCorner)
-];
 
 const haitu = new MapLayer(
   "船讯网 - 海图",
